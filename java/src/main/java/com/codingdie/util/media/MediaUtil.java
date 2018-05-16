f6f904d28403f49185b2121d5e7bfc78ef6b4e6c
codingdie <547912355@qq.com>
2018-05-16 10:10:54 +0800

    update

--- java/src/main/java/com/codingdie/util/media/MediaUtil.java
+++ java/src/main/java/com/codingdie/util/media/MediaUtil.java
@@ -1,15 +1,38 @@
 package com.codingdie.util.media;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.lang.reflect.Field;
+
 public class MediaUtil {
 
     static {
-        System.load("libmedia");
+        System.setProperty("java.library.path", System.getProperty("java.library.path")
+                + ":/usr/local/lib/");
+        Field fieldSysPath = null;
+        try {
+            fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+            fieldSysPath.setAccessible(true);
+            fieldSysPath.set(null, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.loadLibrary("media");
     }
     private static native String getVideoInfo(String filePath);
 
+    public static  VideoInfo parseVideo(String filePath){
+        String videoInfo = getVideoInfo(filePath);
+        if(videoInfo!=null&&videoInfo.length()>0&&!videoInfo.equals("null")){
+            return JSONObject.parseObject(videoInfo,VideoInfo.class);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println(getVideoInfo("/Users/codingdie/Desktop/video/test.mp4"));
+        System.out.println(parseVideo("/Users/codingdie/Desktop/video/test.mp4"));
     }
 }
 
-//com/codingdie/util/Users/codingdie/Documents/code/JMedia/java/out/production/classes/media
